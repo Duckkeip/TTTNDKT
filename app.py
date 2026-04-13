7206646032d7c98254f8a8dbf52bf7347200bc07
TranPhamMinhDuc <doggygerman@gmail.com>
2026-04-13 13:16:33 +0700

    a

--- app.py
+++ app.py
@@ -773,6 +773,14 @@ def get_ice_servers():
         return [
             {"urls": ["stun:stun.l.google.com:19302"]},
             {"urls": ["stun:stun1.l.google.com:19302"]},
+            {"urls": ["stun:stun2.l.google.com:19302"]},
+            {"urls": ["stun:stun3.l.google.com:19302"]},
+            {"urls": ["stun:stun4.l.google.com:19302"]},
+            {"urls": ["stun:stun.ekiga.net"]},
+            {"urls": ["stun:stun.ideasip.com"]},
+            {"urls": ["stun:stun.schlund.de"]},
+            {"urls": ["stun:stun.voiparound.com"]},
+            {"urls": ["stun:stun.voipbuster.com"]},
         ]
 
 yolo_plate, yolo_sv, reader = load_models()
